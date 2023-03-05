1cde9df64278c899f26d87780e8b9c2c69aafc25
xVelkow <redamaktoum@gmail.com>
2023-03-06 00:40:11 +0100

    Member management
    I created a Model/Controller/Request/Migration/View for the Member manipulations with the resource methods

---
+++ app/Http/Requests/MemberRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+class MemberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(Request $r): array
+    {
+        return [
+            $r->validate([
+                'cefMember'=>['bail','string','required','between:13,13','alpha_num'],
+                'firstNameMember'=>['bail','string','required','max:100'],
+                'lastNameMember'=>['bail','string','required','max:100'],
+                'emailMember'=>['bail','email','ends_with:@ofppt-edu.ma','required','max:100'],
+                'groupMember'=>['bail','string','required','max:25'],
+                'departmentMember'=>['bail','string','required','max:100'],
+                'roleMember'=>['bail','string','required','max:30']
+            ])
+        ];
+    }
+}
