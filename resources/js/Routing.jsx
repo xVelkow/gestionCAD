b4f3a78d59f30aeb104c11466411865cb4c946e5
xVelkow <redamaktoum@gmail.com>
2023-04-02 16:53:43 +0000

    looping through routes

--- resources/js/Routing.jsx
+++ resources/js/Routing.jsx
@@ -5,31 +5,23 @@ import Index from "./pages/Members/Index";
 import Show from "./pages/Members/Show";
 import Edit from "./pages/Members/Edit";
 import NotFound from "./pages/NotFound";
+import React from "react";
 const Routing = () =>{
+    const models = ['Members','Sessions','Plannings','Posts'];
     return(
         <>
             <Routes>
                 <Route path='/Dashboard' element={<Dashboard />} />
-                <Route path='/Dashboard/Members' element={<Index section='Members' />} />
-                <Route path='/Dashboard/Members/:id' element={<Show section='Members' />} />
-                <Route path='/Dashboard/Members/Create' element={<Create section='Members' />} />
-                <Route path='/Dashboard/Members/:id/Edit' element={<Edit section='Members' />} />
-
-                <Route path='/Dashboard/Sessions' element={<Index section='Sessions' />} />
-                <Route path='/Dashboard/Sessions/:id' element={<Show section='Sessions' />} />
-                <Route path='/Dashboard/Sessions/Create' element={<Create section='Sessions' />} />
-                <Route path='/Dashboard/Sessions/:id/Edit' element={<Edit section='Sessions' />} />
-
-                <Route path='/Dashboard/Plannings' element={<Index section='Plannings' />} />
-                <Route path='/Dashboard/Plannings/:id' element={<Show section='Plannings' />} />
-                <Route path='/Dashboard/Plannings/Create' element={<Create section='Plannings' />} />
-                <Route path='/Dashboard/Plannings/:id/Edit' element={<Edit section='Plannings' />} />
-
-                <Route path='/Dashboard/Posts' element={<Index section='Posts' />} />
-                <Route path='/Dashboard/Posts/:id' element={<Show section='Posts' />} />
-                <Route path='/Dashboard/Posts/Create' element={<Create section='Posts' />} />
-                <Route path='/Dashboard/Posts/:id/Edit' element={<Edit section='Posts' />} />
-
+                {
+                    models.map(route=>{return(
+                        <React.Fragment key={`${route}`}>
+                            <Route key={`${route}-index`} path={`/Dashboard/${route}`} element={<Index section={route} />} />
+                            <Route key={`${route}-show`} path={`/Dashboard/${route}/:id`} element={<Show section={route} />} />
+                            <Route key={`${route}-create`} path={`/Dashboard/${route}/Create`} element={<Create section={route} />} />
+                            <Route key={`${route}-edit`} path={`Dashboard/${route}/:id/Edit`} element={<Edit section={route} />} />
+                        </React.Fragment>
+                    )})
+                }
                 <Route path='*' element={<NotFound />} />
             </Routes>
         </>
